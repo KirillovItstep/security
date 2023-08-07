8dd3418b7b7fa0bf7435f2cd80249ed132e57b61
KirillovItStep <kirillov203509@gmail.com>
2023-08-07 21:03:10 +0300

    security

--- security-web/src/main/java/org/itstep/App.java
+++ security-web/src/main/java/org/itstep/App.java
@@ -3,11 +3,11 @@ package org.itstep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-    @SpringBootApplication
-    public class App {
+@SpringBootApplication
+public class App {
 
-        public static void main(String[] args) throws Throwable {
-            SpringApplication.run(App.class, args);
-        }
-
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(App.class, args);
     }
+
+}
